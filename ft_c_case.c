203fcaf89e8cee373f4b40663567c91a88a0f58f
Rodrigo Longhi <rodrigo.longhi@mercadolivre.com>
2021-06-30 23:37:04 -0300

    ft_c_case finished

--- ft_c_case.c
+++ ft_c_case.c
@@ -6,21 +6,70 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 23:10:10 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/06/30 00:02:26 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2021/06/30 23:27:24 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void static	ft_print_space(const char *str, int *chars, int start, int end)
+{
+	int	nbr;
+
+	nbr = ft_atoi(ft_substr(str, start, end));
+	while (nbr > 1)
+	{
+		ft_putchar_fd(' ', 1);
+		nbr--;
+		chars[0]++;
+	}
+	chars[1] += end + 1;
+}
+
+void static	ft_print_zero(const char *str, int *chars, int start, int end)
+{
+	int	nbr;
+
+	nbr = ft_atoi(ft_substr(str, start, end));
+	while (nbr > 1)
+	{
+		ft_putchar_fd('0', 1);
+		nbr--;
+		chars[0]++;
+	}
+	chars[1] += end + 1;
+}
+
+void static	ft_print_char(unsigned char c, int *chars)
+{
+	ft_putchar_fd(c, 1);
+	chars[0]++;
+	chars[1]++;
+}
+
 void	ft_c_case(const char *str, int pos, int arg, int *chars)
 {
-	unsigned char	temp;
+	unsigned char	c;
 
-	temp = (unsigned char)arg;
-	if (str[pos] == 'c')
+	c = (unsigned char)arg;
+	if (pos == 1)
+		ft_print_char(c, chars);
+	else if (str[1] == '-')
 	{
-		ft_putchar_fd(temp, 1);
-		chars[0]++;
-		chars[1]++;
+		ft_print_char(c, chars);
+		if (str[2] == '0')
+			ft_print_zero(str, chars, 3, pos - 1);
+		else
+			ft_print_space(str, chars, 2, pos - 1);
+	}
+	else
+	{
+		if (str[1] == '0')
+			ft_print_zero(str, chars, 2, pos - 1);
+		else
+			ft_print_space(str, chars, 1, pos - 1);
+		ft_print_char(c, chars);
 	}
 }
+
+//%3c
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 19:51:53 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/06/30 00:03:01 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2021/06/30 23:13:33 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ void static	ft_check_args(const char *str, va_list arg, int *chars)
 	while (str[pos])
 	{
 		if (str[pos] == 'c')
+		{
 			ft_c_case(str, pos, va_arg(arg, int), chars);
+			break ;
+		}
 		// else if (str[pos] == 's')
 		// 	return (ft_s_case(str, pos, va_arg(arg, char *)));
 		// else if (str[pos] == 'p')
@@ -48,14 +51,15 @@ int	ft_printf(const char *str, ...)
 
 	va_start(arg, str);
 	chars = ft_calloc(2, sizeof(int));
-	while (str[chars[0] + chars[1]] != '\0')
+	while (str[chars[1]] != '\0')
 	{
-		if (str[chars[0] + chars[1]] == '%')
-			ft_check_args(str + chars[0] + chars[1], arg, chars);
+		if (str[chars[1]] == '%')
+			ft_check_args(str + chars[1], arg, chars);
 		else
 		{
-			ft_putchar_fd(str[chars[0] + chars[1]], 1);
+			ft_putchar_fd(str[chars[1]], 1);
 			chars[0]++;
+			chars[1]++;
 		}
 	}
 	va_end(arg);
