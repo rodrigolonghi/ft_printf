ab17169d1bc36fad50abc2817d9f472b742e6c41
Rodrigo Longhi <rodrigo.longhi@mercadolivre.com>
2021-07-08 23:48:59 -0300

    Added extra function to find a number in a string

--- ft_find_nbr.c
+++ ft_find_nbr.c
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_find_nbr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/06/23 19:51:01 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/07/08 23:48:41 by rfelipe-         ###   ########.fr       */
+/*   Created: 2021/07/08 23:08:39 by rfelipe-          #+#    #+#             */
+/*   Updated: 2021/07/08 23:32:42 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
-# include "./libft/libft.h"
+int	ft_find_nbr(const char *str)
+{
+	int	start;
+	int	len;
 
-int	ft_find_nbr(const char *str);
-int	ft_printf(const char *str, ...);
-int	ft_c_case(const char *str, int pos, va_list arg, int *chars);
-int	ft_percent_case(const char *str, int pos, int *chars);
-int	ft_s_case(const char *str, int pos, char *arg, int *chars);
-
-#endif
+	if (!str)
+		return (-1);
+	start = 0;
+	if (str[0] == '-' || str[0] == '.')
+		start++;
+	len = 0;
+	while (ft_isalpha(str[start]))
+		start++;
+	while (ft_isdigit(str[len + start]))
+		len++;
+	return (ft_atoi(ft_substr(str, start, len)));
+}
